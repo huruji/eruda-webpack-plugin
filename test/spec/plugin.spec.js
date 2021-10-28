9c5d3ae996a228252c2046019c06c8a4ed30bf53
spomni <mikhail.snitko@gmail.com>
2021-10-28 10:53:21 +0300

    Draft non-prodaction mode test case.

--- test/spec/plugin.spec.js
+++ test/spec/plugin.spec.js
@@ -1,6 +1,19 @@
 describe('ErudaWebpackPlugin', () => {
 
-  it('Should do nothing if the webpack mode is not "development".')
+  it('Should do nothing if the webpack mode is not "development".', async () => {
+    const cleanCompiler = webpack(cleanConfig)
+    const extendCompiler = webpack(merge(cleanConfig, baseConfig, {
+      mode: 'prodaction'
+    }))
+
+    await runCompiler(cleanCompiler)
+    const cleanContent = readFileSync('../boilerplate/dist/main.js')
+    
+    await runCompiler(extendCompiler)
+    const extendContent = readFileSync('../boilerplate/dist/main.js')
+    
+    assert.strictEqual(cleanContent, extendContent)
+  })
 
   it('Should inject eruda code into .js output files.')
 })
