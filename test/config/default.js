cf15f34fe256699479647a071b7b42ff7d6f7a10
spomni <mikhail.snitko@gmail.com>
2021-10-28 12:31:07 +0300

    Code default config and inject them into the.plugin.spec.js.

---
+++ test/config/default.js
@@ -0,0 +1,7 @@
+const ErudaHtmlPlugin = require('../../lib/plugin')
+
+module.exports = {
+  plugins: [
+    new ErudaHtmlPlugin()
+  ]
+}
--- test/spec/plugin.spec.js
+++ test/spec/plugin.spec.js
@@ -1,14 +1,15 @@
 const webpack = require('webpack')
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 
 const cleanConfig = require('../boilerplate/webpack.config.js')
+const defaultConfig = require('../config/default')
 
 describe('ErudaWebpackPlugin', () => {
 
   it('Should do nothing if the webpack mode is not "development".', async () => {
     const cleanCompiler = webpack(cleanConfig)
-    const extendCompiler = webpack(merge(cleanConfig, baseConfig, {
-      mode: 'prodaction'
+    const extendCompiler = webpack(merge(cleanConfig, defaultConfig, {
+      mode: 'production'
     }))
 
     await runCompiler(cleanCompiler)
